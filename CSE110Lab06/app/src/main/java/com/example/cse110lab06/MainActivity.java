1d52398fee83975af9716740d19af763629eba9d
Raymond <Raymond07xia@gmail.com>
2023-02-21 17:46:05 -0800

    second

--- CSE110Lab06/app/src/main/java/com/example/cse110lab06/MainActivity.java
+++ CSE110Lab06/app/src/main/java/com/example/cse110lab06/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println(12);
+//        System.out.println(12);
         Intent intent = new Intent(this, TodoListActivity.class);
         startActivity(intent);
     }
--- CSE110Lab06/app/src/main/java/com/example/cse110lab06/TodoListItem.java
+++ CSE110Lab06/app/src/main/java/com/example/cse110lab06/TodoListItem.java
@@ -1,4 +1,15 @@
 package com.example.cse110lab06;
 
 public class TodoListItem {
+    public long id = 0;
+    public String text;
+    public boolean completed;
+    public int order;
+
+    TodoListItem(String text, boolean completed, int order) {
+        this.text = text;
+        this.completed = completed;
+        this.order = order;
+    }
+
 }
