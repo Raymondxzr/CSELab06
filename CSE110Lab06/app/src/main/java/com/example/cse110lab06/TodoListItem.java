c157f94d9a416778bb31e1f76b757bcb830c3d60
Raymond <Raymond07xia@gmail.com>
2023-02-22 15:11:05 -0800

    3

--- CSE110Lab06/app/src/main/java/com/example/cse110lab06/TodoListItem.java
+++ CSE110Lab06/app/src/main/java/com/example/cse110lab06/TodoListItem.java
@@ -1,5 +1,17 @@
 package com.example.cse110lab06;
 
+import android.content.Context;
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+
 public class TodoListItem {
     public long id = 0;
     public String text;
@@ -12,4 +24,26 @@ public class TodoListItem {
         this.order = order;
     }
 
+    @Override
+    public String toString() {
+        return "TodoListItem{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", completed=" + completed +
+                ", order=" + order +
+                '}';
+    }
+
+    public static List<TodoListItem> loadJSON(Context context, String path) {
+        try {
+            InputStream input = context.getAssets().open(path);
+            Reader reader = new InputStreamReader(input);
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<TodoListItem>>() {}.getType();
+            return gson.fromJson(reader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
 }
